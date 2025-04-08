06e31b1ebde2217c3ad6481c55cd96bf2c25c2d9
Mason <leemason@hawaii.edu>
2025-04-07 23:20:08 -1000

    mockup of the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 
 /** The Home page. */
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
+      <Row className="h-100 align-items-center justify-content-center text-center">
         <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+          <h1>Welcome to Campus Careers!</h1>
+          <h2>Connect with Internships and Jobs tailored to you</h2>
         </Col>
       </Row>
+      <div className="d-grid gap-3 col-8 mx-auto my-4">
+        <Button variant="outline-primary" size="lg">➜ Explore Companies</Button>
+        <Button variant="outline-primary" size="lg">➜ Browse Student Profiles</Button>
+        <Button variant="outline-primary" size="lg">➜ Learn How It Works</Button>
+      </div>
+      <h3 className="fw-bold mt-5 text-center">Featured Companies</h3>
+      <ul className="list-unstyled text-center">
+        <li>Oceanic Tech</li>
+        <li>Hawaiian Airlines IT Dirision</li>
+        <li>Inovatech</li>
+      </ul>
     </Container>
   </main>
 );
