ef58fd8d5dd399de4d281db0c454fe9831ce3155
Alex Chang <144504277+jang-taeyang@users.noreply.github.com>
2025-04-25 05:04:58 -1000

    yay

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,7 +2,6 @@ import { prisma } from '@/lib/prisma';
 
 export default async function TestDBPage() {
   const skills = await prisma.skillModel.findMany(); // This works if you've run `npx prisma generate`
-
   return (
     <main>
       <h1>Skills from Supabase (SkillModel model)</h1>
