ab0e87a546aaacae3e9a1bf48c78b39b475473f9
Jayden Huynh <jayden.huynh2509@gmail.com>
2025-04-24 23:05:35 -1000

    Update dbActions.ts

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -67,13 +67,14 @@ export async function deleteStuff(id: number) {
  * Creates a new user in the database.
  * @param credentials, an object with the following properties: email, password.
  */
-export async function createUser(credentials: { email: string; password: string }) {
+export async function createUser(credentials: { email: string; password: string; name: string }) {
   // console.log(`createUser data: ${JSON.stringify(credentials, null, 2)}`);
   const password = await hash(credentials.password, 10);
   await prisma.user.create({
     data: {
       email: credentials.email,
       password,
+      name: credentials.name,
     },
   });
 }
