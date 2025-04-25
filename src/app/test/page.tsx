fc48c4cc118c0dffc0c9972a2e0c173838bebdf4
Alex Chang <144504277+jang-taeyang@users.noreply.github.com>
2025-04-25 10:49:19 -1000

    iwjdvbi urvbwiu

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,28 @@
 import { prisma } from '@/lib/prisma';
 
 export default async function TestDBPage() {
-  const skills = await prisma.skillModel.findMany(); // This works if you've run `npx prisma generate`
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      skills: true, // Enum array will be fetched here
+    },
+  });
+
   return (
     <main>
-      <h1>Skills from Supabase (SkillModel model)</h1>
+      <h1>Users with Skills</h1>
       <ul>
-        {skills.map((s) => (
-          <li key={s.id}>{s.name}</li>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}
+            :
+            {user.skills.join(', ')}
+            {' '}
+            {/* Join the skills array */}
+          </li>
         ))}
       </ul>
     </main>
   );
 }
-// test
