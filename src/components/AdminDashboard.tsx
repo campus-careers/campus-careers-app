8876cc580f503198c915a5f593e2c6c711a5f630
Alex Chang <144504277+jang-taeyang@users.noreply.github.com>
2025-04-25 03:32:55 -1000

    Merge branch 'issue-28'

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,32 +1,55 @@
 'use client';
 
-import { adminList } from '@prisma/client';
 import { Card } from 'react-bootstrap';
 
-/* Renders a single row in the List Stuff table. See list/page.tsx. */
-const AdminDashboard = ({ name, skills, interests, location, companies, interviews, image, id }: adminList) => (
+type AdminDashboardProps = {
+  id: string;
+  name: string;
+  image: string;
+  skills: string[];
+  interests: string[];
+  location: string;
+  companies: string[];
+  interviews: string[];
+};
+
+const AdminDashboard = ({
+  name,
+  skills,
+  interests,
+  location,
+  companies,
+  interviews,
+  image,
+  id,
+}: AdminDashboardProps) => (
   <Card className="h-100">
     <Card.Body>
       <Card.Title>{name}</Card.Title>
       <Card.Img variant="top" src={image} width={75} height={75} />
       <Card.Text>
         <strong>Skills:</strong>
+        {' '}
         {skills.join(', ')}
       </Card.Text>
       <Card.Text>
         <strong>Interests:</strong>
+        {' '}
         {interests.join(', ')}
       </Card.Text>
       <Card.Text>
         <strong>Location:</strong>
+        {' '}
         {location}
       </Card.Text>
       <Card.Text>
         <strong>Companies:</strong>
+        {' '}
         {companies.join(', ')}
       </Card.Text>
       <Card.Text>
         <strong>Interviews:</strong>
+        {' '}
         {interviews.join(', ')}
       </Card.Text>
     </Card.Body>
