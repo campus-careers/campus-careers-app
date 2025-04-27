5b11b633b63d897abdac8f0aee7bd33c22ebc93e
Jayden Huynh <jayden.huynh2509@gmail.com>
2025-04-27 00:57:41 -1000

    update eslint

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,9 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export async function GET() {
+export default async function GET() {
   try {
     const users = await prisma.user.findMany();
     return NextResponse.json(users);
