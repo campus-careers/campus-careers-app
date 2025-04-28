585db824cf2693b164c9fd9d3956dd5ff81581b0
Jayden Huynh <jayden.huynh2509@gmail.com>
2025-04-28 04:24:26 -1000

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,30 @@
-import { PrismaClient } from '@prisma/client';
+/* eslint-disable import/no-named-as-default */
+import prisma from '@/lib/prisma';
 
-const prisma = new PrismaClient();
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+export const revalidate = 0; // Disable revalidation
 
-export default prisma;
+export default async function TestDBPage() {
+  if (!process.env.DATABASE_URL) {
+    return (
+      <main>
+        <h1>Database URL not set. Cannot fetch data.</h1>
+      </main>
+    );
+  }
+
+  const users = await prisma.user.findMany();
+
+  // Yes
+  return (
+    <main>
+      <h1>Users from Database:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.email}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
