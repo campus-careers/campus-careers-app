7d7918ffcee81f4afa4876882761784d21205c32
Jayden Huynh <jayden.huynh2509@gmail.com>
2025-04-28 14:20:59 -1000

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,26 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-async function createPrismaClientWithRetry(retries = 5, delay = 2000): Promise<PrismaClient> {
-  for (let attempt = 0; attempt < retries; attempt++) {
-    try {
-      const client = new PrismaClient();
-      await client.$connect();
-      console.log('✅ Prisma connected');
-      return client;
-    } catch (err) {
-      console.error(`❌ Prisma connection failed (attempt ${attempt + 1}/${retries})`);
-      if (attempt < retries - 1) {
-        await new Promise((resolve) => setTimeout(resolve, delay)); // wait before retry
-      } else {
-        throw err; // give up
-      }
-    }
-  }
-  throw new Error('Failed to connect Prisma after retries');
-}
-
-export const prisma = globalForPrisma.prisma ?? await createPrismaClientWithRetry();
+// ignore
+// eslint-disable-next-line import/prefer-default-export, operator-linebreak
+export const prisma =
+  // eslint-disable-next-line operator-linebreak
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // CAM: is this the right level of logging?
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
