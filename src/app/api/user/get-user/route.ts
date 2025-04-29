34b29887125b5b5b808c650b624de33453ceb319
Alex Chang <144504277+jang-taeyang@users.noreply.github.com>
2025-04-29 03:59:35 -1000

    i5.81

--- src/app/api/user/get-user/route.ts
+++ src/app/api/user/get-user/route.ts
@@ -1,17 +1,32 @@
+/* eslint-disable import/prefer-default-export */
+import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { prisma } from '@/lib/prisma';
 import authOptions from '@/lib/authOptions';
+import { prisma } from '@/lib/prisma';
 
-export default async function GET() {
+export async function GET() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) {
-    return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+    return NextResponse.json({ success: false, error: 'Unauthorized' }, { status: 401 });
   }
 
   const user = await prisma.user.findUnique({
     where: { email: session.user.email },
+    select: {
+      name: true,
+      email: true,
+      location: true,
+      skills: true,
+      interests: true,
+      portfolio: true,
+      image: true,
+    },
   });
 
-  return new Response(JSON.stringify(user));
+  if (!user) {
+    return NextResponse.json({ success: false, error: 'User not found' }, { status: 404 });
+  }
+
+  return NextResponse.json({ success: true, user });
 }
